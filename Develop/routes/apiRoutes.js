2266d2f1040398d6a9991adf992b776b8483f9c5
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-09 16:26:45 -0400

    set up the server.js file

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,14 @@
+// require express
+const router = require('express').Router();
+// require the class file notbooks so you can use the functions
+
+// get request for /api/notes
+// call the getNotes() from the class that you required
+// res.json(note)
+
+// post request for /api/notes
+// call the addNotes() from the class that you required
+// res.json(note)
+
+// delete request for /api/notes/:id
+// call the deleteNote() function
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+// require the htmlRoutes and apiRoutes
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+// middleware that is needed to use POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+})
