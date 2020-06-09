d17236a8f09d8785f370705d53d5a1d320773144
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-09 18:09:50 -0400

    set up the apiRoutes

--- Develop/db/notebook.js
+++ Develop/db/notebook.js
@@ -1,16 +1,17 @@
 // require uuidv4 package to generate an id : uuidv4()
+const fs = require('fs');
 
 class Notebook {
 
-    // getNotes()
+    getNotes()
     // inside this function, READ from the db.js file
     // the contents of the json file will be displayed on the page
     // to read the file, use fs.readFile()
 
-    // addNotes()
+    addNotes()
     // inside this function, write to the file using fs.writeFile()
 
-    // deleteNotes()
+    deleteNotes()
     // check against all of the notes to see which one has the id you're looking to delete
     // call getNotes() and then 'filter' the resulsts to find the id you're looking for and return the ones that don't match
 
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,13 +4,24 @@ const router = require('express').Router();
 const Notebook = require('../db/notebook');
 
 // get request for /api/notes
+router.get('/notes', (req, res) => {
+Notebook.getNotes();
+res.json(note)
+});
 // call the getNotes() from the class that you required
 // res.json(note)
 
 // post request for /api/notes
+router.post('/notes', (req, res) => {
+Notebook.addNotes();
+res.json(note)
+});
 // call the addNotes() from the class that you required
 // res.json(note)
 
+router.delete('/notes/:id', (req, res) => {
+Notebook.deleteNote();
+});
 // delete request for /api/notes/:id
 // call the deleteNote() function
 
