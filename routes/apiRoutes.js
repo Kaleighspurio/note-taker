3ab55b8588b5cdaf6a75887435b724521be17e0e
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-14 09:55:38 -0400

    The input notes will now save to the db.json file correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,53 @@
 const router = require("express").Router();
 const Notes = require("../db/notes");
-const fs = require('fs');
-const path = require('path');
-const datafile = path.join(__dirname, '../db/db.json');
+const fs = require("fs");
+const path = require("path");
+const datafile = path.join(__dirname, "../db/db.json");
 const noteData = [];
 
 // get request for /api/notes
 router.get("/notes", (req, res) => {
-//   res.sendFile(datafile);
-//   res.json(note)
+  fs.readFile(datafile, "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.json(data);
+  });
+  //   res.sendFile(datafile);
+  //   res.json(note)
 });
-
 
 // post request for /api/notes
 router.post("/notes", (req, res) => {
-//   console.log(req.body);
+  //   console.log(req.body);
   const noteData = [];
-  noteData.push(fs.readFileSync(datafile));
-  const newNote = JSON.stringify({
-    id: Math.floor(Math.random() * 100001),
-    title: req.body.title,
-    text: req.body.text,
-  });
-//   console.log(JSON.parse(newNote));
-  // console.log(newNote);
-//   const stringNewNote = JSON.stringify(newNote);
-  // console.log(stringNewNote);
-  noteData.push(newNote);
-  console.log(noteData);
+  //   fs.readFileSync(datafile, (err, data) => {
+  //       if (err){
+  //           throw err;
+  //       }
+  //       console.log(data, "this is the console log in the read file");
+  //     //  noteData.push(data);
+  //   })
 
+  const readFile = fs.readFileSync(datafile, "utf8");
+  console.log(readFile, "this is the console to look for");
+  const parsedFile = JSON.parse(readFile);
+  console.log(parsedFile, "this is hopefully a parsed file");
+  noteData.push(readFile);
+  let newNote = req.body;
+  newNote.id = Math.floor(Math.random() * 100001);
+  parsedFile.push(newNote);
+  console.log(parsedFile, "this is the log after the push");
+  const stringNotes = JSON.stringify(parsedFile);
+  console.log(stringNotes, "These should be all the notes in string form");
   const addNotes = () => {
-    fs.writeFile(datafile, noteData, (err) => {
+    fs.writeFile(datafile, stringNotes, (err) => {
       if (err) throw err;
     });
     console.log("the note has been saved.");
   };
   addNotes();
-  
-  // push to the db.json
-  // Notes.addNotes();
-  // res.json(newNote);
+  res.json(stringNotes);
 });
 // call the addNotes() from the class that you required
 // res.json(note)
